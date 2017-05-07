5d98f065ad0828550ffa433eb247e8d4213dfa61
Fernando Daciuk <f.daciuk@gmail.com>
2017-05-07 09:33:56 -0300

    Add first version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log('not ready yet')
+'use strict'
+
+import React, { PureComponent } from 'react'
+import t from 'prop-types'
+
+class LazyImg extends Component {
+  constructor () {
+    super()
+    this.state = { loaded: false }
+    this.img = {}
+
+    this.handleLoad = () => {
+      this.setState({ loaded: true })
+      this.props.onImageLoad()
+    }
+  }
+
+  componentDidMount () {
+    if (this.img.complete && !this.state.loaded) {
+      this.handleLoad()
+    }
+  }
+
+  render () {
+    return (
+      <img src={this.props.src}
+        alt={this.props.alt}
+        onLoad={this.handleLoad}
+        className={
+          `[ lazy-image ] ${this.state.loaded ? '-loaded' : ''}
+          ${this.props.className}`
+        }
+        ref={(img) => {this.img = img}}
+      />
+    )
+  }
+}
+
+LazyImg.defaultProps = {
+  onImageLoad: () => null,
+  className: '',
+  alt: ''
+};
+
+LazyImg.propTypes = {
+  src: t.string.isRequired,
+  className: t.string,
+  onImageLoad: t.func,
+  alt: t.string
+};
+
+export default LazyImg
